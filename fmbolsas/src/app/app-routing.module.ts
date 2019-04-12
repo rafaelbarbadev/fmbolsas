bbd5257e82cc8cd2a39881cd20d104f7ba0e77f8
Rafael Oliveira <rafaelciborgue@hotmail.com>
2019-04-12 01:34:02 -0300

    configurando pwa, boostrap e rotas

--- fmbolsas/src/app/app-routing.module.ts
+++ fmbolsas/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: null }, // default route
+  { path: '', component: null }, // generic route
+  { path: '**', component: null} // 404 page error
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
